ba50d342805795258cccc580a19d9f4145d1924a
Lucas-Mondini <lucasfepereira1@gmail.com>
2023-08-28 22:49:49 -0300

    generator

--- tools/generator/src/crudGenerator.js
+++ tools/generator/src/crudGenerator.js
@@ -3,14 +3,32 @@ import path from "path"
 import fs from "fs"
 import url from "url"
 
+
+
+function capitalizeFirstLetter(inputString) {
+    if (inputString.length === 0) {
+      return inputString; // Handle empty string case
+    }
+    
+    const firstChar = inputString.charAt(0).toUpperCase();
+    const restOfString = inputString.slice(1).toLowerCase();
+  
+    return firstChar + restOfString;
+  }
 export default class CrudGenerator {
 
     templateName = ""
     targetProjectDir = ""
+    targetOutDir = ""
     data = {}
+
+
     constructor(templateName, targetProjectDir) {
-        this.templateName = templateName;
+        this.templateName = capitalizeFirstLetter(templateName.toLowerCase());
+        this.targetOutDir = templateName.toLowerCase()
+          
         this.data = {
+            pathname: this.targetOutDir,
             templateName: this.templateName
         }
 
@@ -20,29 +38,68 @@ export default class CrudGenerator {
 
             this.targetProjectDir  = path.resolve(path.dirname(currentModulePath), '../../../');
         }
-        this.generateModel()
+        
+        this.generateCrud()
+    }
+
+    generateCrud() {
+        this.generateModel();
+        this.generateRepository();
+        this.generateService();
+        this.generateController();
+        this.generateInjector();
+        this.generateRouter();
     }
 
 
-    generate(type) {
+    generate(classname, type = "class") {
         let scriptPath = path.dirname(new URL(import.meta.url).pathname);
         scriptPath = scriptPath.startsWith('/') ? scriptPath.slice('1') : scriptPath;
         
-        const templateFilePath = path.join(scriptPath, `../templates/crud/${type}/index.ejs`);
+        const generateType = type === "class" ? "index" : "interface";
+
+        const templateFilePath = path.join(scriptPath, `../templates/crud/${classname}/${generateType}.ejs`);
         const templateFile = fs.readFileSync(templateFilePath, "utf-8")
         const compiledTemplate = ejs.compile(templateFile)
         const output = compiledTemplate(this.data)
 
-        fs.writeFileSync(`${scriptPath}/out/${this.templateName}.ts`, output, "utf-8")
-        console.log(`${type} to ${this.templateName} generated`)
+        let outpath = `${scriptPath}/out/${classname}/${this.targetOutDir}`
+        if(classname === "injectors") {
+            outpath = `${scriptPath}/out/${classname}/crud/${this.targetOutDir}`
+        }
+
+        if(!fs.existsSync(outpath)) {
+            fs.mkdirSync(outpath, { recursive: true });
+        }
+
+        fs.writeFileSync(`${outpath}/${generateType}.ts`, output, "utf-8")
+        console.log(`${classname} ${generateType} to ${this.templateName} generated`)
     }
 
     async generateModel() {
-        this.generate("model")
+        this.generate("models")
     }
 
     async generateRepository() {
-        this.generate("repositories")
+        this.generate("repositories", "class")
+        this.generate("repositories", "interface")
+    }
+    async generateService() {
+        this.generate("services", "class")
+        this.generate("services", "interface")
+    }
+
+    async generateController() {
+        this.generate("controllers", "class")
+        this.generate("controllers", "interface")
+    }
+
+    async generateInjector() {
+        this.generate("injectors", "class")
+    }
+
+    async generateRouter() {
+        this.generate("routes", "class")
     }
 
 }
